6fec2472463e9331b810c1851a4fb27c1dee96a1
Asrar Ali <aliasrar84@gmail.com>
2024-08-18 20:24:14 +0530

    firstcommit

--- src/components/HomePage/Hero.jsx
+++ src/components/HomePage/Hero.jsx
@@ -5,10 +5,10 @@ const Hero = () => {
       <div className='w-[100%] overflow-hidden'>
         <img className='absolute w-[100%] object-cover object-top h-[775px] max-[1400px]:h-[520px] z-[-1]' src="https://i.pinimg.com/564x/dc/6a/6d/dc6a6db4258fe4a4ba94f8b0964e1bd7.jpg" alt="" />
       </div>
-      <div className='max-[900px]:pb-20 h-[775px] max-[1400px]:h-[520px] z-10 text-white flex text-center flex-col justify-center items-center'>
+      <div className='max-[900px]:pb-10 h-[775px] max-[1400px]:h-[520px] z-10 text-white flex text-center flex-col justify-center items-center'>
         <p className='junge italic text-xl drop-shadow-lg my-5'>Fashion</p>
           <h1 className='max-[1400px]:text-2xl text-5xl tracking-widest drop-shadow-lg leading-[4rem]  font-bold'>PERFECT BUNCH <br /> FOR EVERY OCCASION</h1>
-          <button className='bg-red-600 text-white font-semibold py-5 px-10 mt-10'>Shop Now</button>
+          <button className='bg-red-600 text-white font-semibold py-5 px-10 mt-10 max-[1400px]:px-6 max-[1400px]:py-4 '>Shop Now</button>
       </div>
     </div>
   )
--- src/components/HomePage/NewArrivals.jsx
+++ src/components/HomePage/NewArrivals.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React from 'react';
+import Slider from 'react-slick';
+import Products from '../../products.json';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const NewArrivals = () => {
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
-    <div>NewArrivals</div>
-  )
+    <div className='mx-[140px] max-[1400px]:mx-[10px]'>
+      <div className='mt-10'>
+        <h2 className='text-center text-2xl'>New Arrivals</h2>
+      </div>
+      <div className='mt-10'>
+        <div className='hidden md:flex flex-wrap gap-6 justify-center items-center'>
+          {Products.map(product => (
+            <div className='my-5' key={product.key}>
+              <img className='w-[268px] h-[322px] max-[1400px]:w-[156px] max-[1400px]:h-[171px]' src={product.imageLink} alt={product.title} />
+              <h2 className='text-center font-semibold text-xl my-5'>{product.title}</h2>
+              <div className='flex justify-center items-center gap-10'>
+                <p>${product.discountedPrice}.00</p>
+                <p className='font-light text-red-600 line-through'>${product.price}.00</p>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className='md:hidden'>
+          <Slider {...settings}>
+            {[Products.slice(0, 4), Products.slice(4, 8)].map((group, index) => (
+              <div key={index}>
+                <div className='flex flex-wrap justify-center items-center gap-6'>
+                  {group.map(product => (
+                    <div className='my-5' key={product.key}>
+                      <img className='w-[268px] h-[322px] max-[1400px]:w-[156px] max-[1400px]:h-[171px]' src={product.imageLink} alt={product.title} />
+                      <h2 className='text-center font-semibold text-xl my-5 max-[1400px]:text-lg'>{product.title}</h2>
+                      <div className='max-[1400px]:gap-4 flex justify-center items-center gap-10'>
+                        <p className='max-[1400px]:text-sm'>${product.discountedPrice}.00</p>
+                        <p className='max-[1400px]:text-sm font-light text-red-600 line-through'>${product.price}.00</p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </Slider>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default NewArrivals
+export default NewArrivals;
